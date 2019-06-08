f5fdcd668b627f199febea50fb6421a478d3ffe7
Stanislav Sergienya <skilful522@gmail.com>
2019-06-08 12:52:07 +0300

    second commit

--- src/com/horizon/Main.java
+++ src/com/horizon/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello GitHub");
         System.out.println("новое изменение");
+        System.out.println("change");
     }
 }
