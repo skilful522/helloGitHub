75fdcf8b86227203ca2e5bda10651c455b49c660
Stanislav Sergienya <skilful522@gmail.com>
2019-06-08 12:46:00 +0300

    first commit

---
+++ src/com/horizon/Main.java
@@ -0,0 +1,9 @@
+package com.horizon;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello GitHub");
+        System.out.println("новое изменение");
+    }
+}
